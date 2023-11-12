e208b015662171a90c7afef2012a203d1dd195b1
Yagara <yagara@gmail.com>
2023-11-12 09:19:15 +0600

    Last commit

--- keyboards.py
+++
@@ -1,7 +0,0 @@
-from aiogram import types
-from bot import bot
-
-
-def web_app_keyboard():
-    keybord = types.ReplyKeyboardMarkup(row_width=1)
-    webApp = types.web
--- server.py
+++ server.py
@@ -1,13 +1,23 @@
-from flask import Flask, render_template
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 import sqlite3
 
 
-app = Flask(__name__,
-            static_folder='static',
-            template_folder='templates')
-db = sqlite3.connect('idb.db')
-sql = db.cursor()
+app = FastAPI()
 
-@app.route('')
-def mpList():
-    render_template("index.html")
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
+
+db = sqlite3.connect('my_database.db')
+sql = db.cursor()
+data = sql.execute("""SELECT * FROM events""").fetchall()
+
+@app.get('/')
+def mpList(request: Request):
+    global data
+    context = {
+        "request": request,
+        "data": data
+        }
+    return templates.TemplateResponse(name="index.html", context=context)
